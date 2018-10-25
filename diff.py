7b5210511ea033446567fa95d89f1e9bcdba14cf
Anand prabhakar <anandprabhakar0507@gmail.com>
2018-10-24 19:01:24 -0700

    create diff.py
    main script

---
+++ diff.py
@@ -0,0 +1,10 @@
+# This program subtract two numbers
+
+a = 7
+d = 2
+
+# Add two numbers
+diff = float(a) - float(d)
+
+# Display the Difference
+print('The diff of {0} and {1} is {2}'.format(a, d, diff))
